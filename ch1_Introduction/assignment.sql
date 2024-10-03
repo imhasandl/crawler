12a1ce1e4cc83ca8739f073ad6f739eac3bdf817
imhasandl <imhasandl04@gmail.com>
2024-10-04 01:42:41 +0500

    sql first steps

---
+++ ch1_Introduction/assignment.sql
@@ -0,0 +1,7 @@
+-- CashPal
+-- In this course, we will be building the database for a pretend PayPal clone called CashPal! Storing information related to people's money, transactions, and identity is very important! So we will need to make sure we use proper conventions to build a safe, and reliable database architecture that our users can rely on.
+
+-- Assignment
+-- I have provided a simple SQL statement for you that retrieves some records from a table. However there isn't a people table, the table in our database is called users. Fix the bug by changing people to users within the SELECT statement.
+
+SELECT * FROM users;
