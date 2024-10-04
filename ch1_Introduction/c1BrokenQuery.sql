28a9f98dcfaf003696812de8d3b6b15fefa8d36b
imhasandl <imhasandl04@gmail.com>
2024-10-05 01:04:28 +0500

    c1 broken query

---
+++ ch1_Introduction/c1BrokenQuery.sql
@@ -0,0 +1,7 @@
+-- Broken Query
+-- Your boss at CashPal wants to get a list of names for all the users in the database so he can thank them for joining. After hours of trying, he is insistent that the database must be broken and has asked you to take a look at his query.
+
+-- Challenge
+-- Fix the error in the query so that it returns all the user names from the database.
+
+SELECT name FROM users;
