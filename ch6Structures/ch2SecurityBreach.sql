06b6d6c85734753a3391a803cbf34747bf5c62de
imhasandl <imhasandl04@gmail.com>
2024-10-23 22:44:25 +0500

    new project

--- ch6Structures/ch2SecurityBreach.sql
+++
@@ -1,9 +0,0 @@
--- Security Breach
--- Bad news! CashPal suffered its first security breach, one of the interns leaked a sensitive file that contained some user passwords. Even worse, the user table has passwords stored as plain text. Luckily, we know which passwords were leaked so we can warn these specific users before we move to a modern password storage system.
-
--- Challenge
--- Write a query that returns the name and username for every user with a password equal to backendDev, welovebootdev, or SQLrocks. Order the records so that the names are in alphabetical order.
-
-SELECT name, username FROM users
-WHERE password IN ("backendDev", "welovebootdev", "SQLrocks")
-ORDER BY name
--- ch7Aggregations/ch1AverageAges.sql
+++
@@ -1,9 +0,0 @@
--- Average Ages
--- The CashPal marketing team was able to optimize their advertising with the help of the user data we pulled. However, they now want you to query the average age of users for each country that the business operates in, not just the United States.
-
--- Challenge
--- Write an SQL statement that returns two columns, the country_code and the average age of users for records with that country_code. The marketing team has asked that we round the average to the nearest whole number and rename the column that contains the average age to average_age.
-
-
-SELECT country_code, round(avg(age)) AS average_age FROM users
-GROUP BY country_code
