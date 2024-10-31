c3b60b1f4c78eb0ae3d6ae770964685fdefce957
hasan <imhasandl04@gmail.com>
2024-11-01 03:29:27 +0500

    get html function

--- main.go
+++ main.go
@@ -17,4 +17,30 @@ func main(){
 	rawBaseURL := os.Args[1]
 
 	fmt.Printf("starting crawl of: %s...\n", rawBaseURL)
+
+	htmlBody, err := getHTML(rawBaseURL)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	fmt.Println(htmlBody)
 }
+
+
+// go run .
+// Expecting exit code: 1
+// Expecting stdout to contain all of:
+// no website provided
+
+
+// go run . "https://website1.com" "website2.com"
+// Expecting exit code: 1
+// Expecting stdout to contain all of:
+// too many arguments provided
+
+
+// go run . "https://website2.com"
+// Expecting exit code: 0
+// Expecting stdout to contain all of:
+// starting crawl
+// https://website2.com
